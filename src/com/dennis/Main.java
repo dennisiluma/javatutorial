8ca1d1e442bd4184a2284b6bf702a3305fa55105
Sproxil <sproxil@Sproxils-MacBook-Pro.local>
2022-07-28 11:59:45 +0100

    printed my first  'Hello'

---
+++ src/com/dennis/Main.java
@@ -0,0 +1,9 @@
+package com.dennis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, This is our first App!!");
+    }
+}
